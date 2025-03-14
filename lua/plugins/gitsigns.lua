2b9427927bd108c146ea11cadfa476ad2c82113c
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-03-14 09:08:47 -0400

    feat(plugins): add gitsigns plugin

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      attach_to_untracked = true,
+      sign_priority = 14,
+    },
+  },
+}
