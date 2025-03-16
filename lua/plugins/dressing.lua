e37e21b185754367d705354e2d6f5d9946420d08
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-03-16 08:45:49 -0400

    feat(plugin-dressing): simplify lazy.nvim options

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -2,26 +2,24 @@ return {
   {
     'stevearc/dressing.nvim',
     event = 'VeryLazy',
-    opts = function()
-      return {
-        input = {
-          enabled = false,
-        },
-        select = {
-          enabled = true,
-          backend = 'telescope',
-          get_config = function(opts)
-            local res = {}
-            local themes = require('telescope.themes')
-            if opts.kind == 'codeaction' then
-              res.telescope = themes.get_cursor()
-            else
-              res.telescope = themes.get_dropdown()
-            end
-            return res
-          end,
-        },
-      }
-    end,
+    opts = {
+      input = {
+        enabled = false,
+      },
+      select = {
+        enabled = true,
+        backend = 'telescope',
+        get_config = function(opts)
+          local res = {}
+          local themes = require('telescope.themes')
+          if opts.kind == 'codeaction' then
+            res.telescope = themes.get_cursor()
+          else
+            res.telescope = themes.get_dropdown()
+          end
+          return res
+        end,
+      },
+    },
   },
 }
