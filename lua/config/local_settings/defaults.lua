4d7dca770164c25063c0ce4d12b48d4fd12aebdc
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-03-19 10:35:46 -0400

    refactor(local-settings): extract default settings into new file

---
+++ lua/config/local_settings/defaults.lua
@@ -0,0 +1,42 @@
+local defaults = {}
+local enable_hints_fts = {
+  'rust',
+  'typescript',
+  'typescriptreact',
+  'svelte',
+}
+local format_on_save_fts = {
+  'c',
+  'cpp',
+  'css',
+  'gdscript',
+  'go',
+  'html',
+  'javascript',
+  'javascriptreact',
+  'json',
+  'lua',
+  'markdown',
+  'python',
+  'rust',
+  'scss',
+  'svelte',
+  'typescript',
+  'typescriptreact',
+  'xml',
+  'zig',
+}
+for _, ft in ipairs(enable_hints_fts) do
+  if defaults[ft] == nil then
+    defaults[ft] = {}
+  end
+  defaults[ft].lsp_enable_hints = true
+end
+for _, ft in ipairs(format_on_save_fts) do
+  if defaults[ft] == nil then
+    defaults[ft] = {}
+  end
+  defaults[ft].format_on_save = true
+end
+
+return defaults
--- lua/config/local_settings/file.lua
+++ lua/config/local_settings/file.lua
@@ -1,45 +1,6 @@
 local M = {}
 
-local default_settings = {}
-local enable_hints_fts = {
-  'rust',
-  'typescript',
-  'typescriptreact',
-  'svelte',
-}
-local format_on_save_fts = {
-  'c',
-  'cpp',
-  'css',
-  'gdscript',
-  'go',
-  'html',
-  'javascript',
-  'javascriptreact',
-  'json',
-  'lua',
-  'markdown',
-  'python',
-  'rust',
-  'scss',
-  'svelte',
-  'typescript',
-  'typescriptreact',
-  'xml',
-  'zig',
-}
-for _, ft in ipairs(enable_hints_fts) do
-  if default_settings[ft] == nil then
-    default_settings[ft] = {}
-  end
-  default_settings[ft].lsp_enable_hints = true
-end
-for _, ft in ipairs(format_on_save_fts) do
-  if default_settings[ft] == nil then
-    default_settings[ft] = {}
-  end
-  default_settings[ft].format_on_save = true
-end
+local defaults = require('config.local_settings.defaults')
 
 local function get_file_path()
   local path = vim.fn.expand('%:p:h')
@@ -58,21 +19,21 @@ end
 
 function M.read_settings()
   local p = get_file_path()
-  local defaults = vim.deepcopy(default_settings)
+  local d = vim.deepcopy(defaults)
   if p == nil then
-    return defaults
+    return d
   end
   local f = io.open(p, 'rb')
   if f == nil then
-    return defaults
+    return d
   end
   local j = f:read('*a')
   f:close()
   local ok, settings = pcall(vim.json.decode, j)
   if ok then
-    return vim.tbl_deep_extend('force', defaults, settings)
+    return vim.tbl_deep_extend('force', d, settings)
   end
-  return defaults
+  return d
 end
 
 return M
