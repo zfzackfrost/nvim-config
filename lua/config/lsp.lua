29f084a28c0955ba1aab7738458791f8d574f69e
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-03-19 10:39:37 -0400

    refactor(lsp): add lsp-specific keymaps on LspAttach

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -6,8 +6,33 @@ local function on_attach(args)
   -- Only create mappings for the first client
   if not vim.b._has_lsp_maps then
     local wk = require('which-key')
+
+    local function toggle_hints()
+      local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = args.buf })
+      vim.lsp.inlay_hint.enable(not enabled, { bufnr = args.buf })
+    end
     wk.add({
       { '<leader>cr', ':IncRename ', buffer = args.buf, desc = 'Rename under cursor' },
+      {
+        '<leader>cs',
+        [[<Cmd>Telescope lsp_document_symbols theme=ivy<Cr>]],
+        desc = 'Symbols',
+      },
+      {
+        '<leader>cf',
+        '<Cmd>Format<Cr>',
+        desc = 'Format code',
+      },
+      {
+        '<leader>ca',
+        vim.lsp.buf.code_action,
+        desc = 'Code action',
+      },
+      {
+        '<leader>ch',
+        toggle_hints,
+        desc = 'Toggle hints,',
+      },
     })
     vim.b._has_lsp_maps = true
   end
