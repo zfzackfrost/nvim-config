1258eb183129b00289112447ed2c4527645d3394
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-03-26 08:41:13 -0400

    fix(maps): add descriptions to `git` mapping group

--- lua/config/keymaps/git.lua
+++ lua/config/keymaps/git.lua
@@ -1,7 +1,21 @@
 local wk = require('which-key')
+
+local prefix = '<leader>g'
+local prefix_add = prefix .. 'a'
+local prefix_commit = prefix .. 'c'
+
+---@type wk.Icon
+local git_icon = {
+  icon = '',
+  color = 'red',
+}
 wk.add({
-  { '<leader>g', group = 'git' },
-  { '<leader>gaa', [[<Cmd>Git add --all<Cr>]] },
-  { '<leader>gaf', [[<Cmd>Git add %<Cr>]] },
-  { '<leader>gc', [[<Cmd>Git commit<Cr>]] },
+  { prefix, group = 'git' },
+
+  { prefix_add, group = 'add', icon = git_icon },
+  { prefix_add .. 'a', [[<Cmd>Git add --all<Cr>]], desc = 'Add all', icon = git_icon },
+  { prefix_add .. 'f', [[<Cmd>Git add %<Cr>]], desc = 'Add current file', icon = git_icon },
+
+  { prefix_commit, group = 'commit', icon = git_icon },
+  { prefix_commit .. 'c', [[<Cmd>Git commit<Cr>]], desc = 'Commit', icon = git_icon },
 })
