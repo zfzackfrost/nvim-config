33e82b5ba394db8d97399c41ca11df88eeace48e
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-04-06 08:22:48 -0400

    feat(commands): add `silent` support for `view` user-commands

--- lua/config/commands/views.lua
+++ lua/config/commands/views.lua
@@ -1,11 +1,14 @@
-local function clean_views()
+---@param t vim.api.keyset.create_user_command.command_args
+local function clean_views(t)
   local views = vim.fs.find(function()
     return true
   end, { path = vim.go.viewdir, type = 'file', limit = math.huge })
   for _, v in ipairs(views) do
     vim.uv.fs_unlink(v)
   end
-  vim.notify(string.format('Cleaned %d views', #views), vim.log.levels.INFO, {})
+  if not t.smods.silent and not t.smods.emsg_silent then
+    vim.notify(string.format('Cleaned %d views', #views), vim.log.levels.INFO, {})
+  end
 end
 
 vim.api.nvim_create_user_command('CleanViews', clean_views, { bar = true })
