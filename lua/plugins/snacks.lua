53fcecde52fa5d1efa8ae8abcc11b5fb0615bb44
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-04-09 09:24:44 -0400

    feat(snacks): use `snacks.nvim` for `vim.ui.select` implementation

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,5 +9,9 @@ return {
       dashboard = { enabled = true },
       notifier = { enabled = true },
     },
+    config = function(_, opts)
+      require('snacks').setup(opts)
+      vim.ui.select = Snacks.picker.select
+    end,
   },
 }
