96334af6b12004e09ea1f335bee276ad0f5e0988
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-04-10 08:26:17 -0400

    docs(maps): add misc. comments

--- lua/config/keymaps/remap.lua
+++ lua/config/keymaps/remap.lua
@@ -1,6 +1,9 @@
 local m = require('utils.map')
 
+-- Disable tmux prefix key
 m.disable_map('<C-a>')
+
+-- Force `mini.surround` mappings to show with `which-key`
 vim.keymap.set({ 'n', 'v' }, 's', function()
   require('which-key').show({
     keys = 's',
