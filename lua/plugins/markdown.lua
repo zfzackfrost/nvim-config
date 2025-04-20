ba77b467fd6794d4859b80a1862c99afa5f8f75a
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-04-20 10:34:18 -0400

    fix(plugins): markdown preview lazy loading

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,20 +1,6 @@
 return {
   {
     'iamcco/markdown-preview.nvim',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    event = 'VeryLazy',
-    build = function()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'LazyLoad',
-        callback = function(args)
-          if args.data == 'markdown-preview.nvim' then
-            vim.fn['mkdp#util#install']()
-            return true
-          end
-        end,
-      })
-    end,
+    ft = { 'markdown' },
   },
 }
