532d3dd1f75989b3e400b785208a9a801c8d7291
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-05-10 07:21:43 -0400

    feat(lsp): apply lsp settings correctly for nvim 0.11

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -2,5 +2,6 @@ require('config.lsp.attach')
 local servers = require('config.lsp.servers')
 local lspconfig = require('lspconfig')
 for _, name in ipairs(servers.all_servers()) do
-  lspconfig[name].setup(servers.get_opts(name))
+  vim.lsp.config(name, servers.get_opts(name))
+  vim.lsp.enable(name)
 end
