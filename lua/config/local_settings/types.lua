4d9fa6be64727882386a23b8f618abfe8a51e55c
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-05-13 07:58:09 -0400

    feat(local-settings): minor change to types, for clarity

--- lua/config/local_settings/types.lua
+++ lua/config/local_settings/types.lua
@@ -1,5 +1,7 @@
 ---@alias LspFormatMode 'never'|'fallback'|'first'|'last'|'prefer'
 
+---@alias FiletypeName string a possible value of `vim.bo.filetype`
+
 ---@class FiletypeSettings
 ---@field lsp_enable_hints? boolean If `true` enable inlay hints for this filetype. If `false` disable. If `nil`, do nothing.
 ---@field autostart_server? boolean If `true` start the Neovim server when opening this filetype.
@@ -7,4 +9,4 @@
 ---@field formatters? string|string[] If not `nil` override the defined formatters for this filetype.
 ---@field lsp_format? LspFormatMode If not `nil`, sets how the language server is used when formatting this filetype.
 
----@alias LocalSettings table<string, FiletypeSettings>
+---@alias LocalSettings table<FiletypeName, FiletypeSettings>
