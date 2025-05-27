edf98d3096f27642c6193d9598241bff503aea94
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-05-27 10:15:27 -0400

    feat(mason): update mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -20,7 +20,9 @@ return {
     'williamboman/mason-lspconfig.nvim',
     dependencies = { 'williamboman/mason.nvim' },
     opts = {
-      automatic_installation = true,
+      automatic_enable = {
+        exclude = { 'rust_analyzer' },
+      },
     },
   },
 }
