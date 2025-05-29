0527a7bd4caed5a186d8ab814e44cd9740c74d24
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-05-29 09:50:39 -0400

    fix(lsp): remove unnecessary `require` of `lspconfig`

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,6 +1,5 @@
 require('config.lsp.attach')
 local servers = require('config.lsp.servers')
-local lspconfig = require('lspconfig')
 for _, name in ipairs(servers.all_servers()) do
   vim.lsp.config(name, servers.get_opts(name))
   vim.lsp.enable(name)
