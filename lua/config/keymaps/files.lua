c64441bcbb4390f0977bc5ef8cd513a7ad995eea
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-06-01 07:44:42 -0400

    feat(keymaps): add mapping to change filetype with `telescope`

--- lua/config/keymaps/files.lua
+++ lua/config/keymaps/files.lua
@@ -2,4 +2,5 @@ local wk = require('which-key')
 wk.add({
   { '<leader>f', group = 'files' },
   { '<leader>fp', [[:!mkdir -p %:p:h<Cr>]], desc = 'Make parent directory' },
+  { '<leader>ft', [[<Cmd>Telescope filetypes<Cr>]], desc = 'Set filetype' },
 })
