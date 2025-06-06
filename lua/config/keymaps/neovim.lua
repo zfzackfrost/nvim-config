db7490cbb3de061cf53f0ae51052bbcb838406cb
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-06-06 07:08:37 -0400

    refactor(keymaps): re-name and re-organize `(neo)vim` mappings

--- lua/config/keymaps/neovim.lua
+++ lua/config/keymaps/neovim.lua
@@ -1,10 +1,15 @@
 local wk = require('which-key')
+
+local prefix = '<leader>v'
+local prefix_maintain = prefix .. 'm'
+local prefix_server = prefix .. 's'
 wk.add({
   { '<leader>v', group = '(neo)vim' },
-  { '<leader>vd', [[<Cmd>CleanViews<Cr>]], desc = 'Remove all views' },
+  { prefix_maintain, group = 'maintain' },
+  { prefix_maintain .. 'v', [[<Cmd>CleanViews<Cr>]], desc = 'Remove all views' },
 
-  { '<leader>vs', group = 'server' },
-  { '<leader>vss', [[<Cmd>StartServer<Cr>]], desc = 'Start server' },
-  { '<leader>vse', [[<Cmd>StopServer<Cr>]], desc = 'Stop Server' },
-  { '<leader>vsd', [[<Cmd>CleanServerPipe<Cr>]], desc = 'Clean old server pipe' },
+  { prefix_server, group = 'server' },
+  { prefix_server .. 's', [[<Cmd>StartServer<Cr>]], desc = 'Start server' },
+  { prefix_server .. 'e', [[<Cmd>StopServer<Cr>]], desc = 'Stop Server' },
+  { prefix_server .. 'd', [[<Cmd>CleanServerPipe<Cr>]], desc = 'Clean old server pipe' },
 })
