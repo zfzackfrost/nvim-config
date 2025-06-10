e0c92206b486a96db811f3dd31a1f7d8b9d9d977
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-06-10 08:06:22 -0400

    feat(mappings): add icon for `list` mapping group

--- lua/config/keymaps/list.lua
+++ lua/config/keymaps/list.lua
@@ -1,9 +1,30 @@
 local wk = require('which-key')
 local prefix = '<leader>l'
+
+---@type wk.Icon
+local list_icon = {
+  icon = ' ',
+  color = 'azure',
+}
+---@type wk.Icon
+local diagnostics_icon = {
+  icon = '󱖫',
+  color = 'yellow',
+}
 wk.add({
-  { prefix, group = 'list' },
-  { prefix .. 'x', '<Cmd>Trouble diagnostics toggle<Cr>', desc = 'Diagnostics' },
-  { prefix .. 'X', '<Cmd>Trouble diagnostics toggle filter.buf=0<Cr>', desc = 'Buffer Diagnostics' },
+  { prefix, group = 'list', icon = list_icon },
+  {
+    prefix .. 'x',
+    '<Cmd>Trouble diagnostics toggle<Cr>',
+    desc = 'Diagnostics',
+    icon = diagnostics_icon,
+  },
+  {
+    prefix .. 'X',
+    '<Cmd>Trouble diagnostics toggle filter.buf=0<Cr>',
+    desc = 'Buffer Diagnostics',
+    icon = diagnostics_icon,
+  },
   { prefix .. 'l', '<Cmd>Trouble loclist toggle<Cr>', desc = 'Location list (Trouble)' },
   { prefix .. 'q', '<Cmd>Trouble qflist toggle<Cr>', desc = 'Quickfix list (Trouble)' },
 })
