7950f9ae761cc7c1c398e953c92f1a497f8115f8
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-07-12 11:33:23 -0400

    feat(markdown): lazy loading for `render_markdown.nvim`

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,8 @@
 return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+    ft = { 'markdown' },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {
