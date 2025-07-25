f6c75d6a1ee8143044e2ee2a5499f019dba1b160
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-07-25 07:15:46 -0400

    feat(options): use lsp folding as default and treesitter as fallback

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,8 +5,6 @@ local opt = vim.opt
 
 opt.viewoptions = { 'folds', 'cursor' }
 opt.formatexpr = "v:lua.require'conform'.formatexpr()"
-opt.foldmethod = 'expr'
-opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 opt.foldlevel = 99
 opt.showmode = false
 opt.clipboard = ''
@@ -16,6 +14,23 @@ opt.signcolumn = 'yes'
 opt.termguicolors = true
 opt.number = true
 opt.relativenumber = true
+
+vim.o.foldmethod = 'expr'
+-- Default to treesitter folding
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client:supports_method('textDocument/foldingRange') then
+      local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+    end
+  end,
+})
 
 -- Diagnostic signs
 vim.diagnostic.config({
