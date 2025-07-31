7570881c44b533d73ac5a97908dc999d81f978b5
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-07-31 07:50:19 -0400

    revert(options)!: no longer use lsp-based folding

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,19 +16,6 @@ vim.o.relativenumber = true
 vim.o.foldmethod = 'expr'
 -- Default to treesitter folding
 vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
--- Prefer LSP folding if client supports it
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client == nil then
-      return
-    end
-    if client:supports_method('textDocument/foldingRange') then
-      local win = vim.api.nvim_get_current_win()
-      vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
-    end
-  end,
-})
 
 -- Diagnostic signs
 vim.diagnostic.config({
