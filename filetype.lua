06f6fa033db755384e6fc063d8bc4a7ec17df4f0
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-08-08 08:07:56 -0400

    feat(filetype): add filetype config for SPICE circuit analysis

--- filetype.lua
+++ filetype.lua
@@ -1,5 +1,13 @@
 vim.filetype.add({
   extension = {
     ['scad'] = 'openscad',
+    -- SPICE circuit/netlist/module/library
+    ['spi'] = 'spice',
+    ['cir'] = 'spice',
+    ['ckt'] = 'spice',
+    ['lib'] = 'spice',
+    ['mod'] = 'spice',
+    ['inc'] = 'spice',
+    ['sub'] = 'spice',
   },
 })
