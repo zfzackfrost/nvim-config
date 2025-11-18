e5ed4026b6991c7707476cf22dcd22f162c15dc4
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-11-18 11:14:42 -0500

    feat(keymaps): improve icons for `<leader>vm` map prefix

--- lua/config/keymaps/neovim.lua
+++ lua/config/keymaps/neovim.lua
@@ -9,12 +9,22 @@ local vim_icon = {
   icon = '',
   color = 'green',
 }
+---@type wk.Icon
+local maintain_icon = {
+  icon = ' ',
+  color = 'grey',
+}
+---@type wk.Icon
+local remove_icon = {
+  icon = ' ',
+  color = 'red',
+}
 
 wk.add({
   { '<leader>v', group = '(neo)vim', icon = vim_icon },
-  { prefix_maintain, group = 'maintain' },
-  { prefix_maintain .. 'v', [[<Cmd>CleanViews<Cr>]], desc = 'Remove all views' },
-  { prefix_maintain .. 's', [[<Cmd>CleanSwap<Cr>]], desc = 'Remove all swap files' },
+  { prefix_maintain, group = 'maintain', icon = maintain_icon },
+  { prefix_maintain .. 'v', [[<Cmd>CleanViews<Cr>]], desc = 'Remove all views', icon = remove_icon },
+  { prefix_maintain .. 's', [[<Cmd>CleanSwap<Cr>]], desc = 'Remove all swap files', icon = remove_icon },
 
   { prefix_server, group = 'server' },
   { prefix_server .. 's', [[<Cmd>StartServer<Cr>]], desc = 'Start server' },
