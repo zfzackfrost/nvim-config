c846124c2a3284a9639f000eeae5cccaaa02bb8f
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-11-18 11:34:12 -0500

    feat(commands): add confirmation prompt for `CleanSwap` and `CleanView`

--- lua/config/commands/swap.lua
+++ lua/config/commands/swap.lua
@@ -2,32 +2,35 @@ local iff = require('utils.func').iff
 
 ---@param t vim.api.keyset.create_user_command.command_args
 local function clean_swap(t)
-  local files = vim.fs.find(function()
-    return true
-  end, { path = vim.go.directory, type = 'file', limit = math.huge })
-  local ok_count = 0
-  local err_count = 0
-  for _, f in ipairs(files) do
-    local _, err = vim.uv.fs_unlink(f)
-    if err == nil then
-      ok_count = ok_count + 1
-    else
-      err_count = err_count + 1
+  local function impl()
+    local files = vim.fs.find(function()
+      return true
+    end, { path = vim.go.directory, type = 'file', limit = math.huge })
+    local ok_count = 0
+    local err_count = 0
+    for _, f in ipairs(files) do
+      local _, err = vim.uv.fs_unlink(f)
+      if err == nil then
+        ok_count = ok_count + 1
+      else
+        err_count = err_count + 1
+      end
+    end
+    if not t.smods.silent and not t.smods.emsg_silent then
+      vim.notify(
+        string.format(
+          'Removed %d swap %s\nEncountered %d %s',
+          ok_count,
+          iff(ok_count == 1, 'file', 'files'),
+          err_count,
+          iff(err_count == 1, 'error', 'errors')
+        ),
+        vim.log.levels.INFO,
+        {}
+      )
     end
   end
-  if not t.smods.silent and not t.smods.emsg_silent then
-    vim.notify(
-      string.format(
-        'Removed %d swap %s\nEncountered %d %s',
-        ok_count,
-        iff(ok_count == 1, 'file', 'files'),
-        err_count,
-        iff(err_count == 1, 'error', 'errors')
-      ),
-      vim.log.levels.INFO,
-      {}
-    )
-  end
+  Snacks.picker.util.confirm("Delete swap files (can't be undone)?", impl)
 end
 
 nvim.create_user_command('CleanSwap', clean_swap, { bar = true })
--- lua/config/commands/views.lua
+++ lua/config/commands/views.lua
@@ -2,33 +2,36 @@ local iff = require('utils.func').iff
 
 ---@param t vim.api.keyset.create_user_command.command_args
 local function clean_views(t)
-  local files = vim.fs.find(function()
-    return true
-  end, { path = vim.go.viewdir, type = 'file', limit = math.huge })
+  local function impl()
+    local files = vim.fs.find(function()
+      return true
+    end, { path = vim.go.viewdir, type = 'file', limit = math.huge })
 
-  local ok_count = 0
-  local err_count = 0
-  for _, f in ipairs(files) do
-    local _, err = vim.uv.fs_unlink(f)
-    if err == nil then
-      ok_count = ok_count + 1
-    else
-      err_count = err_count + 1
+    local ok_count = 0
+    local err_count = 0
+    for _, f in ipairs(files) do
+      local _, err = vim.uv.fs_unlink(f)
+      if err == nil then
+        ok_count = ok_count + 1
+      else
+        err_count = err_count + 1
+      end
+    end
+    if not t.smods.silent and not t.smods.emsg_silent then
+      vim.notify(
+        string.format(
+          'Removed %d view %s\nEncountered %d %s',
+          ok_count,
+          iff(ok_count == 1, 'file', 'files'),
+          err_count,
+          iff(err_count == 1, 'error', 'errors')
+        ),
+        vim.log.levels.INFO,
+        {}
+      )
     end
   end
-  if not t.smods.silent and not t.smods.emsg_silent then
-    vim.notify(
-      string.format(
-        'Removed %d view %s\nEncountered %d %s',
-        ok_count,
-        iff(ok_count == 1, 'file', 'files'),
-        err_count,
-        iff(err_count == 1, 'error', 'errors')
-      ),
-      vim.log.levels.INFO,
-      {}
-    )
-  end
+  Snacks.picker.util.confirm("Delete view files (can't be undone)?", impl)
 end
 
 nvim.create_user_command('CleanViews', clean_views, { bar = true })
