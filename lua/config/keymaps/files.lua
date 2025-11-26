b7fef1c9d76737130c30b0588722fc3952eb3496
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-11-26 10:40:36 -0500

    feat(keymaps): add keymap to set filetype using picker

--- lua/config/keymaps/files.lua
+++ lua/config/keymaps/files.lua
@@ -1,7 +1,20 @@
 local wk = require('which-key')
 
+local function select_filetype()
+  local filetypes = vim.fn.getcompletion('', 'filetype')
+  vim.ui.select(filetypes, {
+    prompt = 'Select filetype',
+  }, function(item)
+    if item == nil then
+      return
+    end
+    vim.bo.filetype = item
+  end)
+end
+
 local prefix = '<leader>f'
 wk.add({
   { prefix, group = 'files' },
   { prefix .. 'p', [[:!mkdir -p %:p:h<Cr>]], desc = 'Make parent directory' },
+  { prefix .. 't', select_filetype, desc = 'Select filetype' },
 })
