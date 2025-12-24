c610746dc4e3bddb578cbf8d7a86daad00534c47
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-12-24 10:29:14 -0500

    feat(mini.operators): disable unused operators

--- lua/plugins/mini/operators.lua
+++ lua/plugins/mini/operators.lua
@@ -4,43 +4,31 @@ return {
     version = false,
     lazy = false,
     opts = {
-      -- Each entry configures one operator.
-      -- `prefix` defines keys mapped during `setup()`: in Normal mode
-      -- to operate on textobject and line, in Visual - on selection.
-
-      -- Evaluate text and replace with output
-      evaluate = {
-        prefix = 'g=',
-        -- Function which does the evaluation
-        func = nil,
-      },
-
       -- Exchange text regions
       exchange = {
         prefix = 'gX',
-        -- Whether to reindent new text to match previous indent
         reindent_linewise = true,
-      },
-
-      -- Multiply (duplicate) text
-      multiply = {
-        prefix = 'gm',
-        -- Function which can modify text before multiplying
-        func = nil,
       },
 
       -- Replace text with register
       replace = {
         prefix = 'gr',
-        -- Whether to reindent new text to match previous indent
         reindent_linewise = true,
       },
 
       -- Sort text
       sort = {
         prefix = 'gs',
-        -- Function which does the sort
-        func = nil,
+      },
+
+      -- Disabled: Multiply (duplicate) text
+      multiply = {
+        prefix = '',
+      },
+
+      -- Disabled: Evaluate text and replace with output
+      evaluate = {
+        prefix = '',
       },
     },
   },
