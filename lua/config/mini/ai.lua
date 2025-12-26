514844b9f06050bc5c51663e3b508d042a2beaf8
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-12-26 07:20:31 -0500

    feat(mini.ai)!: remove unused custom textobjects

--- lua/config/mini/ai.lua
+++ lua/config/mini/ai.lua
@@ -51,14 +51,6 @@ local function current_line(ai)
   return { from = from, to = to }
 end
 
--- ~~~~~~~~~~~ Treesitter Specs ~~~~~~~~~~~
-
-local ts_F = spec_treesitter({ a = '@function.outer', i = '@function.inner' })
-local ts_O = spec_treesitter({
-  a = { '@class.outer', '@conditional.outer', '@loop.outer', '@block.outer' },
-  i = { '@class.inner', '@conditional.inner', '@loop.inner', '@block.inner' },
-})
-
 -- ~~~~~~~~~~~ Textobject Table ~~~~~~~~~~~
 
 -- Table with textobject id as fields, textobject specification as values.
@@ -66,10 +58,6 @@ local ts_O = spec_treesitter({
 local custom_textobjects = {
   g = entire_buffer,
   l = current_line,
-
-  -- Treesitter specs
-  F = ts_F,
-  O = ts_O,
 }
 
 -- ============================================================
