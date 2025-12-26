aae383af0412bd64ec899c60b802d96f1879c8ae
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2025-12-26 06:59:24 -0500

    feat(mini.comment): configure lazy loading

--- lua/plugins/mini/comment.lua
+++ lua/plugins/mini/comment.lua
@@ -2,5 +2,18 @@ return {
   {
     'nvim-mini/mini.comment',
     version = false,
+    keys = {
+      { 'gc', desc = 'Comment', mode = { 'n' } },
+      { 'gc', desc = 'Comment', mode = { 'x' } },
+      { 'gc', desc = 'Comment textobject', mode = { 'o' } },
+    },
+    opts = {
+      mappings = {
+        comment = 'gc',
+        comment_line = 'gcc',
+        comment_visual = 'gc',
+        textobject = 'gc',
+      },
+    },
   },
 }
