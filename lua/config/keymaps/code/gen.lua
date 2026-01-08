986071eadd19d8a7ef96cfb0c52bea78f54c72dd
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-01-08 09:34:23 -0500

    feat(keymaps): add mapping for `GenModeline` command

--- lua/config/keymaps/code/gen.lua
+++ lua/config/keymaps/code/gen.lua
@@ -2,11 +2,17 @@ local wk = require('which-key')
 local m = require('utils.vim.map')
 
 local prefix = '<leader>cg'
+---@type wk.Icon
+local modeline_icon = {
+  icon = ' ',
+  color = 'grey',
+}
 wk.add({
   { prefix, group = 'generate', icon = {
     icon = '󰷫',
     color = 'blue',
   } },
   { prefix .. 'b', [[<Cmd>CommentBox<Cr>]], desc = 'Comment box', mode = { 'n', 'v' } },
+  { prefix .. 'm', [[<Cmd>GenModeline<Cr>]], desc = 'Modeline', mode = { 'n' }, icon = modeline_icon },
   { prefix .. 'd', m.unimplemented, desc = 'Documentation comment', mode = { 'n', 'v' } },
 })
