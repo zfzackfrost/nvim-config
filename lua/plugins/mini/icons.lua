311114a212c8d929f3d52e53562af22f3f2ba9fb
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-02-02 13:57:34 -0500

    feat(mini.icons): set highlights for in icon config

--- lua/plugins/mini/icons.lua
+++ lua/plugins/mini/icons.lua
@@ -5,14 +5,15 @@ return {
     lazy = true,
     opts = {
       directory = {
-        ['.nvim'] = { glyph = '' },
+        ['.nvim'] = { glyph = '', hl = 'MiniIconsGreen' },
         ['snippets'] = { glyph = '', hl = 'MiniIconsGrey' },
       },
       file = {
-        ['init.lua'] = { glyph = '󰢱' },
+        ['init.lua'] = { glyph = '󰢱', hl = 'MiniIconsAzure' },
       },
       extension = {
-        ['snippets'] = { glyph = '' },
+        ['snippets'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['lua'] = { glyph = '󰢱', hl = 'MiniIconsAzure' },
       },
     },
     config = function(_, opts)
