911097a35920fa4ebd665b907540c3dedc0b1bd2
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-03-30 16:20:02 -0400

    feat(cloak): add plugin `cloak.nvim`

---
+++ lua/plugins/cloak.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'laytan/cloak.nvim',
+    ft = 'env',
+    opts = {
+      cloak_length = 12,
+      patterns = {
+        {
+          file_pattern = '.env*',
+          cloak_pattern = '([_%w]+=).+',
+          replace = '%1',
+        },
+      },
+    },
+  },
+}
