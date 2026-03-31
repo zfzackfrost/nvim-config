8db560d5aca354af634327b9c13855b38793b776
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-03-31 09:11:54 -0400

    feat(lsp): don't manually enable servers; allow mason to handle this

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -2,5 +2,4 @@ require('config.lsp.attach')
 local servers = require('config.lsp.servers')
 for _, name in ipairs(servers.all_servers()) do
   vim.lsp.config(name, servers.get_opts(name))
-  vim.lsp.enable(name)
 end
