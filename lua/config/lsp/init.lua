2d7a1966713ba5fc1c8eb7434f68f4b49089cd52
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-01 13:09:24 -0400

    revert(lsp): manually enable all configured lsp servers

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,5 +1,7 @@
 require('config.lsp.attach')
 local servers = require('config.lsp.servers')
-for _, name in ipairs(servers.all_servers()) do
+local all_servers = servers.all_servers()
+vim.lsp.enable(all_servers, true)
+for _, name in ipairs(all_servers) do
   vim.lsp.config(name, servers.get_opts(name))
 end
