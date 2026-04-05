988a5c21f1bf705062ec5cdf8159e450d818795d
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-05 07:58:43 -0400

    feat(lsp/lazydev): ensure `which-key.nvim` is always in workspace

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,7 @@ return {
       library = {
         'snacks.nvim',
         'nvim-dap-ui',
+        'which-key.nvim',
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
