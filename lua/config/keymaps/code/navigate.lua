3f84d09a1431bb5ab4c9e8d31fe0839b4f2b5132
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-06 08:44:19 -0400

    refactor(keymaps): use `add_with_icon` utility for lsp navigation maps

--- lua/config/keymaps/code/navigate.lua
+++ lua/config/keymaps/code/navigate.lua
@@ -1,4 +1,3 @@
-local wk = require('which-key')
 local m = utils.vim.map
 
 local function list_command(mode)
@@ -6,14 +5,17 @@ local function list_command(mode)
   return '<Cmd>Trouble ' .. args .. '<Cr>'
 end
 local prefix = '<leader>cn'
-wk.add({
-  { prefix, group = 'navigate', icon = {
-    icon = '',
-    color = 'purple',
-  } },
+
+---@type wk.Icon
+local navigate_icon = {
+  icon = '',
+  color = 'purple',
+}
+m.add_with_icon({
+  { prefix, group = 'navigate' },
   { prefix .. 't', list_command('lsp_type_definitions'), desc = 'Type definitions', mode = { 'n', 'v' } },
   { prefix .. 'i', list_command('lsp_implementations'), desc = 'Implementations', mode = { 'n', 'v' } },
   { prefix .. 'd', list_command('lsp_declarations'), desc = 'Declarations', mode = { 'n', 'v' } },
   { prefix .. 'D', list_command('lsp_definitions'), desc = 'Definitions', mode = { 'n', 'v' } },
   { prefix .. 'r', list_command('lsp_references'), desc = 'References', mode = { 'n', 'v' } },
-})
+}, navigate_icon)
