a1e6f9a496d904791267f4b5bf1bfc602c9bfc7a
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-06 08:35:40 -0400

    feat(keymaps): add Smart Close keymap

--- lua/config/keymaps/noprefix.lua
+++ lua/config/keymaps/noprefix.lua
@@ -20,3 +20,21 @@ vim.keymap.set('n', '<Esc>', [[<Cmd>nohlsearch<Cr>]])
 vim.keymap.set('i', '<M-Cr>', function()
   require('luasnip').expand()
 end, { silent = true })
+
+local function smart_close()
+  local buftype = vim.bo.buftype
+  if buftype ~= '' then
+    return
+  end
+  local win_count = #nvim.list_wins()
+  if win_count <= 1 then
+    vim.notify("Can't close last window!", vim.log.levels.WARN)
+    return
+  end
+  if vim.bo.modified then
+    vim.notify("Can't close window with unsaved changes!", vim.log.levels.WARN)
+    return
+  end
+  nvim.win_close(0, false)
+end
+vim.keymap.set('n', 'q', smart_close, {})
