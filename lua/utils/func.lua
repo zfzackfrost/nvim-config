379d5c188b36594d219fe78ecb780b44c0bd6433
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-08 07:25:59 -0400

    feat(utils.func): add `defer_wrap` utility function

--- lua/utils/func.lua
+++ lua/utils/func.lua
@@ -12,4 +12,18 @@ function M.iff(choose_a, a, b)
   return b
 end
 
+---Wrap a call to `vim.defer_fn` in a new function.
+---The returned function accepts vaargs, which it forwards to the callback.
+---@param fn function # Callback to execute after a delay
+---@param timeout integer # Number of milliseconds to wait
+---@return fun(...) # new function that calls `vim.defer_fn` with `fn` and `timeout`.
+function M.defer_wrap(fn, timeout)
+  return function(...)
+    local args = utils.table.pack(...)
+    vim.defer_fn(function()
+      fn(utils.table.unpack(args))
+    end, timeout)
+  end
+end
+
 return M
