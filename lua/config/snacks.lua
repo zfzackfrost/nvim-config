81102dd31462f500ceedadce09e147c60e77eded
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-08 08:07:25 -0400

    feat(snacks): customized dashboard icons

--- lua/config/snacks.lua
+++ lua/config/snacks.lua
@@ -5,9 +5,36 @@
 local opts = {
   input = { enabled = true },
   indent = { enabled = true },
-  dashboard = { enabled = true },
   notifier = { enabled = true },
 }
+
+-- ============================================================
+-- ======================== Dashboard =========================
+-- ============================================================
+
+---@class snacks.dashboard.Config
+local dashboard = {
+  enabled = true,
+  preset = {
+    keys = {
+      { icon = '󰱽 ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+      { icon = '󱪞 ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+      { icon = '󱎸 ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+      { icon = '󱋢 ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+      {
+        icon = ' ',
+        key = 'c',
+        desc = 'Config',
+        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+      },
+      { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+      { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+      { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+    },
+  },
+}
+
+opts.dashboard = dashboard
 
 require('snacks').setup(opts)
 vim.ui.select = Snacks.picker.select
