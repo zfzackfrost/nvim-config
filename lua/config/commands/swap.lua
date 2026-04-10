ebc42f12e522d766bfb26faa88bb73ff0cb93f08
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-10 10:14:09 -0400

    feat(commands): specify `notify_formatter` in CleanSwap and CleanViews

--- lua/config/commands/swap.lua
+++ lua/config/commands/swap.lua
@@ -1,7 +1,8 @@
 ---@param t vim.api.keyset.create_user_command.command_args
 local function clean_swap(t)
   local function impl()
-    utils.fs.clear_dir(vim.go.directory, t.smods.silent or t.smods.emsg_silent)
+    local notify_fmt = utils.fs.make_clear_dir_notify_formatter('swap')
+    utils.fs.clear_dir(vim.go.directory, t.smods.silent or t.smods.emsg_silent, notify_fmt)
   end
   Snacks.picker.util.confirm("Delete swap files (can't be undone)?", impl)
 end
--- lua/config/commands/views.lua
+++ lua/config/commands/views.lua
@@ -1,7 +1,8 @@
 ---@param t vim.api.keyset.create_user_command.command_args
 local function clean_views(t)
   local function impl()
-    utils.fs.clear_dir(vim.go.viewdir, t.smods.silent or t.smods.emsg_silent)
+    local notify_fmt = utils.fs.make_clear_dir_notify_formatter('view')
+    utils.fs.clear_dir(vim.go.viewdir, t.smods.silent or t.smods.emsg_silent, notify_fmt)
   end
   Snacks.picker.util.confirm("Delete view files (can't be undone)?", impl)
 end
