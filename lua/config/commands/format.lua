5d338cf015c0c340c4e72d93710144bbecf7e9e2
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-13 11:25:35 -0400

    style(commands.format): minor re-formatting in `Format` command

--- lua/config/commands/format.lua
+++ lua/config/commands/format.lua
@@ -5,7 +5,7 @@ local function format(args)
   if args.count ~= -1 then
     local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
     range = {
-      start = { args.line1, 0 },
+      ['start'] = { args.line1, 0 },
       ['end'] = { args.line2, end_line:len() },
     }
   end
