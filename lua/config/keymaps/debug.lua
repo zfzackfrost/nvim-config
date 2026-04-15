2e060836596357f7fc8d1a2e4cb85c3c1b69d262
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-15 08:00:35 -0400

    feat(keymaps.debug): add maps to select `nvim-dap-view` tabs

--- lua/config/keymaps/debug.lua
+++ lua/config/keymaps/debug.lua
@@ -24,7 +24,7 @@ wk.add({
   {
     prefix .. 'v',
     open_debug_view,
-    desc = 'Open debugging view',
+    desc = 'Open debugging windows',
   },
   {
     prefix .. 'r',
@@ -61,4 +61,18 @@ wk.add({
     end,
     desc = 'Step out',
   },
+})
+
+local prefix_view = prefix .. 'V'
+utils.vim.map.add_with_icon({
+  { prefix_view, group = 'show-view' },
+  { prefix_view .. 'w', [[<Cmd>DapViewShow watches<Cr>]], desc = 'Show Watches' },
+  { prefix_view .. 's', [[<Cmd>DapViewShow scopes<Cr>]], desc = 'Show Scopes' },
+  { prefix_view .. 'e', [[<Cmd>DapViewShow exceptions<Cr>]], desc = 'Show Exceptions' },
+  { prefix_view .. 'b', [[<Cmd>DapViewShow breakpoints<Cr>]], desc = 'Show Breakpoints' },
+  { prefix_view .. 't', [[<Cmd>DapViewShow threads<Cr>]], desc = 'Show Threads' },
+  { prefix_view .. 'r', [[<Cmd>DapViewShow repl<Cr>]], desc = 'Show REPL' },
+}, {
+  icon = '󰈈',
+  color = 'azure',
 })
