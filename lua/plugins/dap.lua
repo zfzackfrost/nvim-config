461879f6d48201e933bf7ee6d483575aab3ef331
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-15 08:20:54 -0400

    fix(dap): unused dependency in plugin spec for `nvim-dap`

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,12 +1,10 @@
 return {
   {
     'mfussenegger/nvim-dap',
-    dependencies = { 'rcarriga/nvim-dap-ui' },
     cmd = { 'DapNew', 'DapTerminate' },
   },
   {
     'igorlfs/nvim-dap-view',
-    -- let the plugin lazy load itself
     lazy = false,
     version = '1.*',
     ---@module 'dap-view'
