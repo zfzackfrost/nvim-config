837c3d7713fadf294a621f7b67cb86769c9b4975
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2026-04-25 08:39:20 -0400

    feat(keymaps.rust): add map to select LSP target

--- lua/config/keymaps/code/language/rust.lua
+++ lua/config/keymaps/code/language/rust.lua
@@ -1,3 +1,20 @@
+local function select_target()
+  local targets = {
+    ['Default'] = '',
+    ['WebASM'] = 'wasm32-unknown-unknown',
+  }
+  local function apply_target(item)
+    if item == nil then
+      return
+    end
+    vim.cmd(("RustAnalyzer config { cargo = { target = '%s' } }"):format(targets[item]))
+  end
+  local labels = vim.tbl_keys(targets)
+  vim.ui.select(labels, {
+    prompt = 'rust-analyzer target',
+  }, apply_target)
+end
+
 return {
   ft = 'rust',
   mappings = {
@@ -16,6 +33,11 @@ return {
       'm',
       '<Cmd>RustLsp expandMacro<Cr>',
       desc = 'Expand macros recursively',
+    },
+    {
+      't',
+      select_target,
+      desc = 'Select LSP target',
     },
   },
 }
